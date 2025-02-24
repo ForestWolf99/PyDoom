c050b37b15c8c8cf618d53a65daf130308a27dad
Forest <wilsonforest99@gmail.com>
2025-02-24 17:51:27 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import pygame as pg
+import sys
+from settings import *
+from map import *
+from player import *
+from raycasting import *
+from object_renderer import *
+
+class Game:
+    def __init__(self):
+        pg.init()
+        pg.mouse.set_visible(False)
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+        self.delta_time = 1
+        self.new_game()
+        pg.display.set_caption('PyDoom')
+
+    def new_game(self):
+        self.map = Map(self)
+        self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
+        self.raycasting = RayCasting(self)
+
+    def update(self):
+        self.player.update()
+        self.raycasting.update()
+        pg.display.flip()
+        self.delta_time = self.clock.tick(FPS)
+        #pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        #self.screen.fill('black')
+        self.object_renderer.draw()
+        #self.map.draw()
+        #self.player.draw()
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.player.mouse_lock = not self.player.mouse_lock
+                    pg.mouse.set_visible(not self.player.mouse_lock)
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
